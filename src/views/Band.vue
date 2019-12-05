eed09161c9a9f62128df2450fbad3e69f8faeffa
willow <jurate.baltrukenaite@gmail.com>
2019-12-05 23:33:35 +0200

    fixed one band loading and started styliing

--- src/views/Band.vue
+++ src/views/Band.vue
@@ -3,7 +3,18 @@
     <b-container>
       <b-row>
         <b-col md="10" offset-md="1">
-          <b-card style="max-width: 800px;">
+          <h1>{{ oneBand.name }}</h1>
+          <b-card-img :src="oneBand.imageUrl" class="rounded-0"></b-card-img>
+          <p>{{ oneBand.description }}</p>
+          <ul>
+            Genre:
+            <li v-for="genre in oneBand.genre" v-bind:key="genre">
+              {{ genre }}
+            </li>
+          </ul>
+          <p>Formed in year: {{ oneBand.formed }}</p>
+
+          <!-- <b-card style="max-width: 800px;">
             <b-row>
               <b-col md="6">
                 <b-card-img
@@ -15,6 +26,7 @@
                 <b-card-body
                   ><h1>{{ oneBand.name }}</h1>
                   <b-card-text>
+                    <p>{{ oneBand.description }}</p>
                     <ul>
                       Genre:
                       <li v-for="genre in oneBand.genre" v-bind:key="genre">
@@ -26,10 +38,10 @@
                 </b-card-body>
               </b-col>
             </b-row>
-          </b-card>
+          </b-card> -->
         </b-col>
       </b-row>
-      <b-button @click="removeBand"  variant="danger">Button</b-button>
+      <b-button @click="removeBand" variant="danger">Button</b-button>
     </b-container>
   </div>
 </template>
@@ -41,17 +53,19 @@ export default {
   data() {
     return {};
   },
-  methods: {
-    ...mapActions(["fetchBands", "deleteBand"]),
-    removeBand(){
-      this.deleteBand(this.$route.params.id)
 
+  methods: {
+    ...mapActions(["fetchBands"]),
+    removeBand() {
+      this.deleteBand(this.$route.params.id);
     }
   },
   computed: {
     ...mapGetters(["allBands"]),
     oneBand() {
-      return this.allBands.find(band => band.id == this.$route.params.id);
+      return this.allBands.find(band => {
+        return band.id.match(this.$route.params.id);
+      });
     }
   },
   created() {
@@ -64,5 +78,27 @@ li {
   display: inline;
   margin: 0 5px 0 0;
   text-align: left;
+}
+
+@media (max-width: 1900px) {
+  .card-img {
+    max-width: 500px;
+    border-radius: calc(0.25rem - 1px);
+    float: left;
+  }
+}
+@media (max-width: 1600px) {
+  .card-img {
+    max-width: 400px;
+    border-radius: calc(0.25rem - 1px);
+    float: left;
+  }
+}
+@media (max-width: 900px) {
+  .card-img {
+    max-width: 200px;
+    border-radius: calc(0.25rem - 1px);
+    float: left;
+  }
 }
 </style>
