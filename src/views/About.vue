cdd423a6ae59f80a02b8afee4122700324cb926c
willow <jurate.baltrukenaite@gmail.com>
2020-01-02 22:36:35 +0200

    added about page background picture to assets

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,8 +12,8 @@
 .about {
   height: 100vh;
   /* background: url("https://picsum.photos/1024/683?image=227") no-repeat center center fixed; */
-  background: url("https://picsum.photos/4096/2722?image=452") no-repeat center
-    center;
+  /* background: url("https://picsum.photos/4096/2722?image=452") no-repeat center */
+  background: url("~@/assets/background.jpg") no-repeat center center;
   background-size: cover;
 }
 .centered-content {
